f7d306bf98e2e0625cd1426d36bc1c3084eb1afd
Muralidharan2211 <42678332+Muralidharan2211@users.noreply.github.com>
2019-07-03 17:18:56 +0530

    Update Stepdefs.java

--- src/test/java/skeleton/Stepdefs.java
+++ src/test/java/skeleton/Stepdefs.java
@@ -6,7 +6,7 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features= {"src/test/resources/skeleton/Register.feature","src/test/resources/skeleton/signin.feature","src/test/resources/skeleton/sc3.feature","src/test/resources/skeleton/addtocart.feature"},glue= "skeleton",
+@CucumberOptions(features= {"src/test/resources/skeleton/signin.feature","src/test/resources/skeleton/sc3.feature","src/test/resources/skeleton/addtocart.feature"},glue= "skeleton",
 plugin="json:target/cucumber.json")
 
 public class Stepdefs {
